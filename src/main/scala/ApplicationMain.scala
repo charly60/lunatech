39c854212de4a83a01429644b383d97093a4bd40
charly <charly.ouin@compte-nickel.fr>
2017-08-17 11:01:30 +0200

    permettre la possibilité de changer le port et l'host pour l'api

--- src/main/scala/ApplicationMain.scala
+++ src/main/scala/ApplicationMain.scala
@@ -3,7 +3,9 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import exoRoutes.RoutesHttp
+
 import scala.concurrent.duration._
+import scala.util.Properties
 
 
 
@@ -16,5 +18,5 @@ object ApplicationMain extends App {
   implicit val executionContext = system.dispatcher
   implicit val timeout = Timeout(10 seconds)
 
-  Http().bindAndHandle(routes.routes, "0.0.0.0", 8080)
+  Http().bindAndHandle(routes.routes, Properties.envOrElse("API_HOST", "0.0.0.0"), Properties.envOrElse("API_PORT", "8080").toInt)
 }
