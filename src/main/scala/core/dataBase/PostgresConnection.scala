3f8502e780c0bbdb6d4dfd24f509db2d5b3d1b88
charly <charly.ouin@compte-nickel.fr>
2017-08-17 10:23:28 +0200

    permettre la possibilité de se connecter à postgres via les variables d'env

--- src/main/scala/core/dataBase/PostgresConnection.scala
+++ src/main/scala/core/dataBase/PostgresConnection.scala
@@ -2,16 +2,16 @@ package core.dataBase
 
 import java.sql.{Connection, DriverManager}
 
-import scala.util.Try
+import scala.util.{Properties, Try}
 
 
 object PostgresConnection {
   def getConnection: Option[Connection] = {
-      val pgHost = "localhost"
-      val pgPort = 5432
-      val pgDBExploitName = "exolunatech"
-      val username = "postgres"
-      val password = "postgres"
+      val pgHost = Properties.envOrElse("POSTGRES_HOST", "localhost")
+      val pgPort = Properties.envOrElse("POSTGRES_PORT", "5432")
+      val pgDBExploitName = Properties.envOrElse("POSTGRES_DB_NAME", "exolunatech")
+      val username = Properties.envOrElse("POSTGRES_USER","postgres")
+      val password = Properties.envOrElse("POSTGRES_PASSWORD","postgres")
       val url = s"jdbc:postgresql://$pgHost:$pgPort/$pgDBExploitName"
 
       Try(DriverManager.getConnection(url, username, password)).toOption
