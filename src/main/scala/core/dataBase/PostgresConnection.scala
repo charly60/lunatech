7f960a68f00d765e49ff9ac3883b21d482daef8c
charly <charly.ouin@compte-nickel.fr>
2017-08-18 17:08:26 +0200

    gère le pool de connexion

--- src/main/scala/core/dataBase/PostgresConnection.scala
+++ src/main/scala/core/dataBase/PostgresConnection.scala
@@ -6,14 +6,26 @@ import scala.util.{Properties, Try}
 
 
 object PostgresConnection {
-  def getConnection: Option[Connection] = {
-    val pgHost = Properties.envOrElse("POSTGRES_HOST", "localhost")
-    val pgPort = Properties.envOrElse("POSTGRES_PORT", "5432")
-    val pgDBExploitName = Properties.envOrElse("POSTGRES_DB_NAME", "exolunatech")
-    val username = Properties.envOrElse("POSTGRES_USER", "postgres")
-    val password = Properties.envOrElse("POSTGRES_PASSWORD", "postgres")
-    val url = s"jdbc:postgresql://$pgHost:$pgPort/$pgDBExploitName"
 
-    Try(DriverManager.getConnection(url, username, password)).toOption
+  var postgresConnection: Option[Connection] = None
+
+
+  def getConnection: Option[Connection] = {
+    postgresConnection match {
+      case None =>
+        val pgHost = Properties.envOrElse("POSTGRES_HOST", "localhost")
+        val pgPort = Properties.envOrElse("POSTGRES_PORT", "5432")
+        val pgDBExploitName = Properties.envOrElse("POSTGRES_DB_NAME", "exolunatech")
+        val username = Properties.envOrElse("POSTGRES_USER", "postgres")
+        val password = Properties.envOrElse("POSTGRES_PASSWORD", "postgres")
+        val url = s"jdbc:postgresql://$pgHost:$pgPort/$pgDBExploitName"
+
+        postgresConnection = Try(DriverManager.getConnection(url, username, password)).toOption
+        postgresConnection
+
+      case Some(Connection) =>
+        postgresConnection
+    }
+
   }
 }
